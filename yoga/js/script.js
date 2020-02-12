05fb0acb7fda5f8af966e4a26d14f7073c0d459d
Ksenia <oks.sloy@gmail.com>
2020-02-12 12:10:46 +0100

    First commit #yoga

---
+++ yoga/js/script.js
@@ -0,0 +1,111 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+    	info = document.querySelector('.info-header'),
+    	tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++){
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event){
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')){
+            for (let i = 0; i < tab.length; i++){
+                if (target == tab[i]){
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Timer
+
+    let deadline = "2020-02-07";									//задаем точку, до которой будет идти отсчет
+
+    function getTimeRemaining(endtime) {
+    	let t = Date.parse(endtime) - Date.parse(new Date()),		//отсчитываем разницу между сейчас и дедлайном
+    		seconds = Math.floor((t/1000) % 60),
+    		minutes = Math.floor((t/1000/60) % 60),
+    		hours = Math.floor((t/(1000*60*60)));
+
+    	//if you need with days:
+
+    	// let hours = Math.floor((t/(1000/60/60) % 24);
+    	// let days = Math.floor((t/(1000*60*60*24)))
+
+    	return {
+    		'total': t,
+    		'hours': hours,
+    		'minutes': minutes,
+    		'seconds': seconds
+    	};
+
+    }
+
+    function setClock(id, endtime) {
+    	let timer = document.getElementById(id),
+    		hours = timer.querySelector('.hours'),
+    		minutes = timer.querySelector('.minutes'),	
+    		seconds = timer.querySelector('.seconds'),
+    		timeInterval = setInterval(updateClock, 1000);
+
+    	function updateClock() {
+    		let t = getTimeRemaining(endtime);
+
+    		function addZero(num){
+                if(num <= 9) {
+                	return '0' + num;
+                } else return num;
+                }
+
+    		hours.textContent = addZero(t.hours);
+    		minutes.textContent = addZero(t.minutes);
+    		seconds.textContent = addZero(t.seconds);
+
+    		if (t.total <= 0) {										//когда закончится время
+    			clearInterval(timeInterval);
+    			hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+    		}
+    	}
+    }
+
+    setClock('timer', deadline);
+
+
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+    	overlay = document.querySelector('.overlay'),
+    	close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function(){			//открыть попап нажатием на кнопку "узнать больше"
+    	overlay.style.display = 'block';				
+    	this.classList.add('more-splash');
+    	document.body.style.overflow = 'hidden';		//залочить скролла страницы
+    });
+
+    close.addEventListener('click', function(){			//закрыть попап нажатием на крестик
+    	overlay.style.display = 'none';
+    	more.classList.remove('more-splash');
+    	document.body.style.overflow = '';				//разлочить скролл страницы
+    })
+});
